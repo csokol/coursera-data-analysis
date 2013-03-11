38d14f3a2333705512f974ac3946b88bbff44487
Francisco Sokol <chico.sokol@gmail.com>
2013-03-10 21:50:47 -0300

    text

--- assignment2/analysis.R
+++ assignment2/analysis.R
@@ -3,7 +3,7 @@ library(randomForest)
 
 load("data/samsungData.rda")
 
-trainingSet = samsungData[samsungData$subject <= 20,] 
+trainingSet = samsungData[samsungData$subject <= 25,] 
 #trainingSet = subset(trainingSet, select=-c(562)) #excluding subject column
 trainingSet = data.frame(trainingSet)
 trainingSet$activity = as.factor(trainingSet$activity)
@@ -12,12 +12,6 @@ testSet = samsungData[samsungData$subject >= 27,]
 #testSet = subset(testSet, select=-c(562)) #excluding subject column
 testSet = data.frame(testSet)
 testSet$activity = as.factor(testSet$activity)
-
-# fBodyAcc.max...
-# tGravityAcc.max...
-# angle.X.gravityMean.
-# fBodyAccMag.max..
-
 
 #plot(x=trainingSet$angle.X.gravityMean., y=trainingSet$angle.Y.gravityMean., col=as.numeric((trainingSet$activity)), pch=19)
 #legend(-0.7,-0.5,legend=unique(trainingSet$activity),col=unique(as.numeric(as.factor(trainingSet$activity))),pch=19)
@@ -28,4 +22,15 @@ testSet$activity = as.factor(testSet$activity)
 #tree1 <- tree(activity ~ . - activity , data=trainingSet)
 
 tree1 <- randomForest(activity ~ . -activity, data = trainingSet, prox=TRUE)
+predictions = predict(tree1, testSet)
+testSetSize = length(predictions)
+correctPredictions = predictions == testSet$activity
 
+errors = table(correctPredictions)[1]
+
+misclassRate = errors/testSetSize
+
+
+
+plot(tree1)
+text(tree1)
